23738cb92f92480bd9a53b1a4612b7a662bc00da
dwiyudha0444 <yudhacamo12@gmail.com>
2023-10-04 14:05:21 +0700

    textspan 3 huruf dalam satu kelas

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AddTextRich());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AddText extends StatelessWidget {
+  const AddText({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +12,41 @@ class MyApp extends StatelessWidget {
         body: Center(
             child: Text(
           'Hello Word',
-          style:
-              TextStyle(fontSize: 35, color: Color.fromARGB(255, 45, 243, 0)),
+          style: TextStyle(
+              fontSize: 35, color: Colors.teal, fontWeight: FontWeight.bold),
         )),
+      ),
+    );
+  }
+}
+
+class AddTextRich extends StatelessWidget {
+  const AddTextRich({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text.rich(TextSpan(
+            text: 'MAD',
+            style: TextStyle(color: Colors.blueGrey),
+            children: [
+              TextSpan(
+                  text: 'Pertemuan',
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.amber)),
+              TextSpan(
+                  text: 'P2',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue))
+            ],
+          )),
+        ),
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:mad_app_p2_kls_a/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AddTextRich());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
